6f81ec3d27043ce19c092780c49b06dede75986d
cyrildou <cyrildou@live.cn>
2025-06-04 18:13:53 +0800

    1st version

---
+++ req_withdraw_test.go
@@ -0,0 +1,37 @@
+package go_mtpay
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWithdraw(t *testing.T) {
+	vLog := VLog{}
+	//构造client
+	cli := NewClient(vLog, MTPayInitParams{MERCHANT_ID, ACCESS_KEY, SECRET_KEY, DEPOST_URL, WITHDRAW_URL, DEPOST_BACK_URL, WITHDRAW_BACK_URL})
+
+	//发请求
+	resp, err := cli.Withdraw(GenWithdrawRequestDemo())
+	if err != nil {
+		fmt.Printf("err:%s\n", err.Error())
+		return
+	}
+	fmt.Printf("resp:%+v\n", resp)
+}
+
+func GenWithdrawRequestDemo() MTPayWithdrawReq {
+	return MTPayWithdrawReq{
+		Client: MTPayClient{
+			RealName: "cy", //客户信息
+		},
+		WithdrawCurrency: "CNY",
+		FiatCurrency:     "CNY",
+		WithdrawAmount:   200.00,
+		MerchantOrderNo:  "793793793",
+		BankCard: BankCard{
+			CardNumber:     "123",
+			BankName:       "工行",
+			BankBranchName: "解放路支行",
+		},
+	}
+}
